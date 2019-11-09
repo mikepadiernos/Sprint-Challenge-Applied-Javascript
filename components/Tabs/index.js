258854edb0426139d6becf10d7ec0fafc28aa6aa
Michael Padiernos <mike.padiernos@gmail.com>
2019-11-08 22:16:55 -0500

    TABS.js: finished code for Tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,42 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+console.log('Promises: Tabs',
+	axios
+		.get('https://lambda-times-backend.herokuapp.com/topics')
+);
+
+function tabs(obj) {
+
+	// ELEMENTS
+	const tTab                      = document.createElement('div');
+
+	// CLASSES
+	tTab.classList.add('tab');
+
+	// CONTENT
+	tTab.textContent                = obj;
+
+	return tTab;
+}
+
+let tabEntryPoint                 = document.querySelector('.topics');
+
+axios
+	.get('https://lambda-times-backend.herokuapp.com/topics')
+	.then(response => {
+		const topics = response.data.topics;
+		console.log('response: topics', topics);
+		// tabEntryPoint.appendChild(tabs(topics));
+		topics.forEach(i => {
+			let ltTopics = tabs(i);
+			tabEntryPoint.appendChild(ltTopics);
+		})
+	})
+	.catch(error => {
+		if (error.includes('Network Error')) {
+			console.log('Network Error');
+		}
+		console.log('No Data Returned');
+	});
